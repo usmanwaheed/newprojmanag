be1114ec9e26bf8f076d72f05882d65fc4b1b05c
usmanwaheed <usmanwaheed.dev@gmail.com>
2025-09-05 17:13:14 +0500

    feat: enhance chat room menu and status

--- server/src/models/UserStatus.js
+++ server/src/models/UserStatus.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userStatusSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    ref: 'UserInfo',
     required: true,
     unique: true
   },
